2a348cf73d5c415de391cc1759fb63795c4aba38
Nick Galvez <nicholas.galvez@gmail.com>
2025-03-04 19:35:19 -0800

    Fix migrate issue during start, add dark theme colors

--- README.md
+++ README.md
@@ -4,15 +4,35 @@ Laravel version with Vue.js frontend using Inertia.js.
 
 ## Installation
 
-1. Clone the repository
+1. Clone the repository and cd into it.
 2. Run `lando start`
-3. Run `lando composer install`
-4. Run `lando artisan migrate`
-5. Run `lando npm install`
-6. Run `lando dev`
+3. Profit
+
+Note: the application uses port 3068 to bind the vite dev server, that can be updated in the lando file
+## Development
+
+Several custom commands are available:
+
+1. Run `lando dev` to start the development server
+2. Run `lando build` to build the assets
+3. Run `lando xdebug-on` to enable xdebug
+4. Run `lando xdebug-off` to disable xdebug
+5. Run `lando npm` to use npm inside the container
+
+Note: although a database service is installed, the application uses SQLite by default.
 
 ## Testing
 
-1. Make a testing .env file: `cp .env.testing.example .env.testing`
+An .env.testing should have already been generated during `lando start` if not, follow these steps:
+
+1. Make an .env.testing file: `cp .env.testing.example .env.testing`
 2. Run `lando artisan key:generate --env=testing` to make a new app key for the testing environment
 3. Run `lando artisan test`
+
+## Next features AKA TODO
+
+If only I had a todo list to keep track of all the things I need to do...
+
+[ ] Sync with Google Tasks
+[ ] Add a pomodoro timer
+[ ] 
--- resources/js/Pages/Todos/Index.vue
+++ resources/js/Pages/Todos/Index.vue
@@ -74,8 +74,9 @@ const triggerConfetti = () => {
         </template>
         <Wrapper>
             <form @submit.prevent="submit" class="flex flex-row flex-wrap items-center justify-start gap-2 lg:w-1/2">
-                <input class="flex-grow" v-model="form.title" type="text" placeholder="New To-Do" />
-                <Button type="submit">Add</Button>
+                <input class="flex-grow dark:bg-gray-700"
+                       v-model="form.title" type="text" placeholder="New To-Do" />
+                <Button class = "dark:border dark:border-gray-700" type="submit">Add</Button>
                 <fieldset class="flex w-full justify-between hidden">
                     <legend>Manage</legend>
                     <Button>Archive Completed</Button>
@@ -83,14 +84,14 @@ const triggerConfetti = () => {
             </form>
             <ul aria-live="polite" class="flex flex-col justify-start lg:w-1/2">
                 <li v-for="todo in todos" :key="todo.id" :class="{
-                    'bg-gray-400 text-gray-500 line-through':
+                    'light:bg-gray-400 text-gray-500 dark:bg-gray-900 line-through':
                         todo.completed,
-                    'text-black': !todo.completed,
-                }" class="my-2 flex items-center justify-between gap-2 pl-2 even:bg-gray-100">
+                    'text-black dark:text-white': !todo.completed,
+                }" class="my-2 flex items-center justify-between gap-2 pl-2 even:bg-gray-100 dark:even:bg-gray-800">
                     <Checkbox :aria-checked="todo.completed.toString()" :checked="todo.completed"
                         @change="updateTodo(todo.id, $event.target.checked)" />
                     {{ todo.title }}
-                    <Button class="ml-auto" @click="deleteTodo(todo.id)">Delete
+                    <Button class="ml-auto dark:border  dark:border-gray-700" @click="deleteTodo(todo.id)">Delete
                     </Button>
                 </li>
             </ul>
